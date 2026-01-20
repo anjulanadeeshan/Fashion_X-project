4eca89af7f9e334391432afacefe7f5309367f35
anjulanadeeshan <mranjula660@gmail.com>
2026-01-20 23:46:23 +0530

    refactor: remove trust badges from newsletter component

--- frontend/src/components/NewsLetter.jsx
+++ frontend/src/components/NewsLetter.jsx
@@ -41,27 +41,7 @@ const NewsLetter = () => {
         </div>
       </form>
       
-      {/* Trust badges */}
-      <div className='flex flex-wrap justify-center items-center gap-8 mt-10 text-gray-500 text-sm'>
-        <div className='flex items-center gap-2'>
-          <svg className='w-5 h-5 text-green-600' fill='currentColor' viewBox='0 0 20 20'>
-            <path fillRule='evenodd' d='M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z' clipRule='evenodd'/>
-          </svg>
-          <span className='font-medium'>100% Secure</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <svg className='w-5 h-5 text-blue-600' fill='currentColor' viewBox='0 0 20 20'>
-            <path fillRule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z' clipRule='evenodd'/>
-          </svg>
-          <span className='font-medium'>No Spam</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <svg className='w-5 h-5 text-red-600' fill='currentColor' viewBox='0 0 20 20'>
-            <path fillRule='evenodd' d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z' clipRule='evenodd'/>
-          </svg>
-          <span className='font-medium'>Unsubscribe Anytime</span>
-        </div>
-      </div>
+      
     </div>
   )
 }
